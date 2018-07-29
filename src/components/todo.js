6f4bd0d35422e657c44133900d04c46fec376825
Meeraj Ahmed <meerajahmed@gmail.com>
2018-07-30 04:11:33 +0530

    todo component

--- src/components/todo.js
+++ src/components/todo.js
@@ -1,3 +1,12 @@
-/**
- * Created by meeahmed on 7/29/2018.
- */
+import React from "react";
+
+const Todo = (props) => (
+  <div>
+    <input type="checkbox" checked={props.todo.completed}
+           onClick={(e) => props.handleToggle(props.todo)} />
+    <span>{props.todo.title}</span>
+    <button onClick={(e) => props.handleDelete(props.todo)}> remove </button>
+  </div>
+);
+
+export default Todo;
