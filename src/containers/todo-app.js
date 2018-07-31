54481c7d4901c298c58c5c9b9040b3bee6222e20
Meeraj Ahmed <meerajahmed@gmail.com>
2018-08-01 02:42:45 +0530

    redux integration

--- src/containers/todo-app.js
+++ src/containers/todo-app.js
@@ -1,3 +1,17 @@
-/**
- * Created by meeahmed on 8/3/2018.
- */
+import {connect} from 'react-redux';
+import TodoApp from '../components/todo-app';
+import {addTodo, updateTodo, removeTodo} from '../actions/todos';
+
+const mapStateToProps = (state) => ({
+  todos: state
+});
+
+const mapDispatchToProps = (dispatch) => (
+  {
+    handleAdd: (todo) => dispatch(addTodo(todo)),
+    handleToggle: (todo) => dispatch(updateTodo(todo)),
+    handleDelete: (todo) => dispatch(removeTodo(todo))
+  }
+);
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoApp);
