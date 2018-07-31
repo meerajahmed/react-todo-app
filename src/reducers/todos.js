489278708d1f72caa425a605299b76ee4ee39788
Meeraj Ahmed <meerajahmed@gmail.com>
2018-08-01 02:42:34 +0530

    redux integration

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,15 +1,26 @@
-const initialState = {
-  todos: []
-};
+import ACTIONS from '../actions';
 
-const reducer = (state = initialState, action) => {
+const defaultTodoState = [];
+
+export default (state = defaultTodoState, action) => {
   switch (action.type) {
-    case 'ADD_TODO':
-      return state;
-    case 'REMOVE_TODO':
-      return state;
-    case 'UPDATE_TODO':
-      return state;
+    case ACTIONS.ADD_TODO:
+      return [...state, action.todo];
+
+    case ACTIONS.REMOVE_TODO:
+      return state.filter((todo) => todo.id !== action.id);
+
+    case ACTIONS.UPDATE_TODO:
+      return state.map((todo) => {
+        if (todo.id === action.todo.id) {
+          return {
+            ...todo,
+            ...action.todo
+          }
+        }
+        return todo;
+      });
+
     default:
       return state;
   }
