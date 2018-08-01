d196401be1df35a414b3599d434d03baab503686
Meeraj Ahmed <meerajahmed@gmail.com>
2018-08-02 02:43:52 +0530

    redux integration

--- src/actions/todos.js
+++ src/actions/todos.js
@@ -1,3 +1,16 @@
-/**
- * Created by meeahmed on 7/30/2018.
- */
+import ACTIONS from './index';
+
+export const addTodo = (todo) => ({
+  type: ACTIONS.ADD_TODO,
+  todo
+});
+
+export const removeTodo = ({id} = {}) => ({
+  type: ACTIONS.REMOVE_TODO,
+  id
+});
+
+export const updateTodo = (todo) => ({
+  type: ACTIONS.UPDATE_TODO,
+  todo
+});
