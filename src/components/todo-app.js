61955ce8d00fdbe5b95838e9b2551b5327bd9741
Meeraj Ahmed <meerajahmed@gmail.com>
2018-08-03 02:44:42 +0530

    redux integration

--- src/components/todo-app.js
+++ src/components/todo-app.js
@@ -1,49 +1,24 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Header from "./header";
 import AddTodo from "./add-todo";
 import TodoList from "./todo-list";
 import Footer from "./footer";
 
-class TodoApp extends Component {
-
-  state = {
-    todos: []
-  };
-
-  title = "todos";
-
-  render() {
-    return (
-      <div>
-        <Header title={this.title} />
-        <AddTodo handleAdd={this.handleAdd} />
-        <TodoList
-          todos={this.state.todos}
-          handleDelete={this.handleDelete}
-          handleToggle={this.handleToggle} />
-        {
-          this.state.todos.length > 0 && <Footer
-            itemsLeft={this.state.todos.filter((todo) => !todo.completed).length} />
-        }
-      </div>
-    );
-  }
-
-  handleAdd = (todo) => {
-    this.setState((prevState) => ({todos: [...prevState.todos, todo]}));
-  };
-
-  handleToggle = (toggleTodo) => {
-    toggleTodo.completed = !toggleTodo.completed;
-    this.setState((prevState) =>
-      ({ todos: prevState.todos.map((todo) => (todo.id === toggleTodo.id) ? toggleTodo : todo)}))
-  };
-
-  handleDelete = (deleteTodo) => {
-    this.setState((prevState) =>
-      ({todos: prevState.todos.filter((todo) => todo.id !== deleteTodo.id)}));
-  };
-
-}
+const TodoApp = (props) => {
+  return (
+    <div>
+      <Header title="todos"/>
+      <AddTodo handleAdd={props.handleAdd}/>
+      <TodoList
+        todos={props.todos}
+        handleDelete={props.handleDelete}
+        handleToggle={props.handleToggle}/>
+      {
+        props.todos.length > 0 && <Footer
+          itemsLeft={props.todos.filter((todo) => !todo.completed).length}/>
+      }
+    </div>
+  );
+};
 
 export default TodoApp;
